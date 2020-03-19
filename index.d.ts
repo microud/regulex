b92cd72c770730f3073ae17330c29bf1b985c25e
microud <gmicroud@gmail.com>
2020-03-20 00:28:21 +0800

    feat: add types definition

---
+++ index.d.ts
@@ -0,0 +1,73 @@
+declare module 'cjs-regulex' {
+
+  export type Node = ExactNode | CharsetNode | GroupNode | ChoiceNode | AssertNode | DotNode | BackrefNode | EmptyNode;
+
+  interface BaseNode {
+    type: string;
+    raw: string;
+    indices: [ number, number ];
+    repeat?: {
+      min: number;
+      max: number;
+      nonGreedy: boolean;
+    }
+  }
+
+  interface ExactNode extends BaseNode {
+    type: 'exact';
+  }
+
+  interface CharsetNode extends BaseNode {
+    type: 'charset';
+    ranges: string[];
+    classes: string[];
+    exclude: boolean;
+  }
+
+  interface GroupNode extends BaseNode {
+    type: 'group';
+    nonCapture?: boolean;
+    num: number;
+    sub: Node[],
+    endParenIndex: number;
+  }
+
+  interface ChoiceNode extends BaseNode {
+    type: 'choice';
+    branches: Array<Node>[];
+  }
+
+  interface AssertNodeWithSub {
+    type: 'assert';
+    assertionType: 'AssertLookahead' | 'AssertNegativeLookahead';
+    sub: Node[];
+  }
+
+  interface AssertNodeWithoutSub {
+    type: 'assert';
+    assertionType: 'AssertNonWordBoundary' | 'AssertWordBoundary' | 'AssertEnd' | 'AssertBegin';
+  }
+
+  type AssertNode = AssertNodeWithSub | AssertNodeWithoutSub;
+
+  interface DotNode {
+    type: 'dot';
+  }
+
+  interface BackrefNode {
+    type: 'backref';
+    num: number;
+  }
+
+  interface EmptyNode {
+    type: 'empty'
+  }
+
+  export interface AST {
+    raw: string;
+    tree: Node[]
+    groupCount: number;
+  }
+
+  export function parse(regex: string): AST;
+}
